ffc3eb628f497a6bec67427fbb54a496e6a5c372
LightningReflex <kyan.wahr@gmail.com>
2025-07-09 20:08:23 +0700

    Fix code inconsistency

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,6 +1,6 @@
 export function fixNbt(nbt: any) {
     for (const key of Object.keys(nbt)) {
         if (nbt[key] === null) delete nbt[key]
-        else if (typeof nbt[key] === 'object' && !Array.isArray(nbt)) fixNbt(nbt[key])
+        else if (typeof nbt[key] === "object" && !Array.isArray(nbt)) fixNbt(nbt[key])
     }
 }
