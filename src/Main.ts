807db0c6db3010f8d317d87ce12097792b8a9b99
LightningReflex <kyan.wahr@gmail.com>
2025-07-09 11:24:44 +0700

    Add email env reading

--- src/Main.ts
+++ src/Main.ts
@@ -8,12 +8,17 @@ export class Main {
 
     public static async main(): Promise<void> {
         let email;
-        do {
-            email = await Terminal.input("What account would you like to log in with (email)?");
-            if (!email) {
-                logger.warn("Email cannot be empty. Please try again.");
-            }
-        } while (!email);
+        if (process.env.ALTERATION_EMAIL) {
+            email = process.env.ALTERATION_EMAIL;
+            logger.info(`Using email from environment variable (ALTERATION_EMAIL): ${email}`);
+        } else {
+            do {
+                email = await Terminal.input("What account would you like to log in with (email)?");
+                if (!email) {
+                    logger.warn("Email cannot be empty. Please try again.");
+                }
+            } while (!email);
+        }
         Main.botAccountManager.addAccount(email, "minehut.com", 25565);
 
         Server.start();
