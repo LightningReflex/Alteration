07b7f33547875e6fd22e487d3dc2e2379a393345
LightningReflex <kyan.wahr@gmail.com>
2025-07-11 22:43:08 +0700

    Add chat history to ChatHandler

--- src/accountmanager/account/handlers/ChatHandler.ts
+++ src/accountmanager/account/handlers/ChatHandler.ts
@@ -3,11 +3,24 @@ import { LogType } from "../BotAccount";
 import Handler from "./Handler";
 import { Bot } from "mineflayer";
 
+type ChatPacket = {
+    name: string;
+    data: object;
+};
+
 export default class ChatHandler extends Handler {
+    private lastChatPackets: ChatPacket[] = []; // Last 300 messages
+
     protected onInit(): void {
         const bot: Bot = this.botAccount.bot!;
         bot.on("message", (message) => {
             this.botAccount.log(LogType.CHAT, message.toAnsi());
+        });
+        bot._client.on("player_chat", (data) => {
+            this.addToChatHistory("player_chat", data);
+        });
+        bot._client.on("system_chat", (data) => {
+            this.addToChatHistory("system_chat", data);
         });
     }
 
@@ -19,5 +32,19 @@ export default class ChatHandler extends Handler {
                 bot._client.chat(data.message);
             }
         });
+    }
+
+    public sendChatHistory(client: ServerClient) {
+        this.lastChatPackets.forEach(packet => {
+            client.write(packet.name, packet.data);
+        });
+    }
+
+    public addToChatHistory(name: string, data: object) {
+        const packet: ChatPacket = { name, data };
+        this.lastChatPackets.push(packet);
+        if (this.lastChatPackets.length > 300) {
+            this.lastChatPackets.shift(); // Remove the oldest packet if we exceed 300
+        }
     }
 }
